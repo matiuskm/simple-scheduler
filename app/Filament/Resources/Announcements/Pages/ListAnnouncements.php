0d9a0bb98ef6be52321b6b5248d14035ed72d31b
lazycoder <matius.km@gmail.com>
2026-01-16 18:13:29 +0700

    add create action to announcements list

--- app/Filament/Resources/Announcements/Pages/ListAnnouncements.php
+++ app/Filament/Resources/Announcements/Pages/ListAnnouncements.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\Announcements\Pages;
 
 use App\Filament\Resources\Announcements\AnnouncementResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListAnnouncements extends ListRecords
 {
     protected static string $resource = AnnouncementResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
