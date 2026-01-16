2c3a41142b2a1a9dae3306c8cfaa0f23ba7da23b
lazycoder <matius.km@gmail.com>
2026-01-16 18:18:58 +0700

    use color picker for announcement background

--- app/Filament/Resources/Announcements/Schemas/AnnouncementForm.php
+++ app/Filament/Resources/Announcements/Schemas/AnnouncementForm.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources\Announcements\Schemas;
 
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Schema;
 
@@ -23,9 +23,8 @@ class AnnouncementForm
                 DatePicker::make('end_at')
                     ->required()
                     ->after('start_at'),
-                TextInput::make('background_color')
+                ColorPicker::make('background_color')
                     ->required()
-                    ->helperText('Hex color, e.g. #1E88E5')
                     ->rule('regex:/^#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$/'),
                 Toggle::make('is_enabled')
                     ->default(true),
